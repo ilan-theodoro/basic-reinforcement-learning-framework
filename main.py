6281795faa59d9bd1cfc03038796830e28e7bd06
Ilan <ilanfsilva@gmail.com>
2023-11-27 23:06:08 -0300

    Deprecate last idea and move forward for painting mnist characters in a grid-world.

--- main.py
+++ main.py
@@ -1,87 +1,54 @@
-from timeit import timeit
-
-import torch, torch.nn as nn, torch.utils.data as data, torchvision as tv
-import lightning as L
-
-from autoencoder import LitAutoEncoder
-
-autoencoder = LitAutoEncoder.load_from_checkpoint("lightning_logs/version_19/checkpoints/epoch=99-step=46900.ckpt")
-
-dataset = tv.datasets.MNIST(".", download=True, transform=tv.transforms.ToTensor(), train=True)
-dataloader = data.DataLoader(dataset, batch_size=len(dataset))
-
-x, y = next(iter(dataloader))
-
-x = x.view(x.size(0), -1).to(autoencoder.device)[:100]
-y = y.to(autoencoder.device)[:100]
-z = autoencoder(x)
-
-# # show pca plot of z
-# from sklearn.decomposition import PCA
-# import matplotlib.pyplot as plt
-#
-# pca = PCA(n_components=2)
-# z_pca = pca.fit_transform(z.detach().cpu().numpy())
-# plt.scatter(z_pca[:, 0], z_pca[:, 1], c=y, cmap="tab10", alpha=0.5, s=4)
-# plt.show()
-#
-#
-# # show umap plot of z
-# import umap
-#
-# z_umap = umap.UMAP().fit_transform(z.detach().cpu().numpy())
-# plt.scatter(z_umap[:, 0], z_umap[:, 1], c=y, cmap="tab10", alpha=0.5, s=4)
-# plt.show()
-
-# compute linear classification with normal equation and one-hot encoding
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import torchvision as tv
 
-# z = z.detach().cpu().numpy()
-# y = y.detach().cpu().numpy()
-# y_onehot = np.zeros((len(y), 10))
-# y_onehot[np.arange(len(y)), y] = 1
-# w = np.linalg.solve(z.T @ z, z.T @ y_onehot)
-# print(w.shape)
-# y_pred = np.argmax(z @ w, axis=1)
-# print("accuracy:", np.mean(y == y_pred))
+np.random.seed(0)
 
-# compute logistic classification using sklearn
-# from sklearn.linear_model import LogisticRegression
-#
-# z = z.detach().cpu().numpy()
-# y = y.detach().cpu().numpy()
-# model = LogisticRegression(max_iter=1000)
-# model.fit(z, y)
-# print(model.coef_.shape)
-# print("accuracy:", model.score(z, y))
+class MNISTEnvironment:
+    def __init__(self, σ_stochasticity=0.1):
+        self.dataset = tv.datasets.MNIST("./data/", download=True)
+        self.state = np.zeros((28, 28))
+        self.image = np.zeros((28, 28))
+        self.reward = np.zeros((28, 28))
+        self.current_position = (0, 0)
+        self.reset()
+        self.σ = σ_stochasticity
 
-# logistic regression with pytorch
-class LogisticRegression(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear = nn.Linear(8, 10)
+    def reset(self):
+        # choose one image
+        self.image = self.dataset[np.random.randint(0, len(self.dataset))][0].numpy()
+        self.state = np.zeros((28, 28))
+        self.reward = (self.image / 255) * 2 - 1
 
-    def forward(self, x):
-        return self.linear(x)
+        # get a random position in the image that is inside the top-25 brightest pixels
+        # TODO make 25 a parameter
+        top_25 = np.argsort(self.image.flatten())[-25:]
+        top_25 = np.unravel_index(top_25, self.image.shape)
+        i = np.random.randint(0, len(top_25[0]))
+        self.current_position = top_25[0][i], top_25[1][i]
 
+    def get_position(self, x, y):
+        μ = np.array([x, y])
+        sample = np.random.multivariate_normal(μ, np.eye(2) * self.σ, 1)[0]
+        sample = np.clip(sample, 0, 27)
+        return sample.astype(int)
 
-model = LogisticRegression().to(autoencoder.device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-1)
+    def eval(self):
+        # return (1 - mse) * 100
+        return 100 * (1 - np.mean((self.state - self.image) ** 2))
 
-# compute the time elapsed
-def opt_it():
-    optimizer.zero_grad()
-    y_pred = model(z)
-    loss = criterion(y_pred, y)
-    loss.backward(retain_graph=True)
-    optimizer.step()
-
-print(timeit(opt_it, number=100))
-
-# for epoch in range(100):
-#
-#     print("loss:", loss.item())
-
-print("accuracy:", np.mean(y.cpu().numpy() == np.argmax(y_pred.detach().cpu().numpy(), axis=1)))
+    def step(self, action):
+        if action == 0:
+            self.state = (self.state[0] - 1, self.state[1])
+        elif action == 1:
+            self.state = (self.state[0] + 1, self.state[1])
+        elif action == 2:
+            self.state = (self.state[0], self.state[1] - 1)
+        elif action == 3:
+            self.state = (self.state[0], self.state[1] + 1)
+        else:
+            raise ValueError("Invalid action")
+        self.state = (max(0, min(self.size - 1, self.state[0])),
+                      max(0, min(self.size - 1, self.state[1])))
+        done = self.state == self.goal
+        reward = 1 if done else 0
+        return self.state, reward, done
